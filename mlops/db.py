e65d8beaa497885d4658bc416efb728288c69434
Abdul Jobayer <mdabduljobayer570@gmail.com>
2026-05-21 23:13:58 +0600

    Dockerize

--- mlops/db.py
+++ mlops/db.py
@@ -2,14 +2,14 @@
 
 import psycopg2
 from datetime import datetime
+import os
 
 DB_CONFIG = {
-    "host": "localhost",
-    "database": "smartml",
-    "user": "postgres",
-    "password": "postgres"
+    "host": os.getenv("DB_HOST", "localhost"),
+    "database": os.getenv("DB_NAME", "smartml"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "postgres")
 }
-
 
 def get_conn():
     return psycopg2.connect(**DB_CONFIG)
